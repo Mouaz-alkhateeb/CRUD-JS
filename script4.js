4ba8900685bc7f61912b5c88afcdbd24d0c7b805
Mouaz alkhateeb <moaz.alkhateeb99@gmail.com>
2022-11-19 01:05:19 +0300

    second commit

---
+++ script4.js
@@ -0,0 +1,14 @@
+let myRequest = new XMLHttpRequest();
+
+myRequest.open("GET", "https://api.github.com/users/elzerowebschool/repos");
+myRequest.send();
+//console.log(myRequest);
+
+myRequest.onreadystatechange = function () {
+    //console.log(myRequest.readyState);
+    //console.log(myRequest.status);
+    if (this.readyState == 4 && this.status == 200) {
+        let data = JSON.parse(this.responseText);
+        console.log(data);
+    }
+}
